e16d6640e3c3f427e5a18fe682dec06327c8dfcb
Cristian <cmioveanu@gmail.com>
2021-06-25 15:44:19 +0100

    change intro and styles

--- src/components/profile.js
+++ src/components/profile.js
@@ -5,23 +5,18 @@ const Profile = ({children}) => {
   return (
     <section id={profile}>
       <h2>Profile</h2>
-      <p>I don’t like to define myself by the work I’ve done. 
-        I define myself by the work I want to do. 
-        Skills can be taught, personality is inherent. 
-        I prefer to keep learning, continue challenging myself, 
-        and do interesting things that matter.</p>
+      <p>I'm an optimistic, energetic and curious person who likes to keep learning, 
+        continue challenging myself, and do interesting things that matter.</p>
 
-      <p>Fueled by high energy levels and boundless enthusiasm, 
-        I’m easily inspired and more then willing to follow my 
-        fascinations wherever they take me. I’m passionate, expressive, 
-        multi-talented spirit with a natural ability to entertain 
-        and inspire. I’m never satisfied to just come up with ideas. 
-        Instead I have an almost impulsive need to act on them.</p>
+        <p>After working in a variety of different jobs, I found myself more 
+          and more interested in tech and often using bits and pieces of software 
+          to make my day to day life easier. That's when I realized that software 
+          development would be a perfect fit for me, and decided to pursue it as a career. </p>
 
-      <p>My abundant energy fuels me in the pursuit of many interests, 
+
+      <p>My enthusiasm also fuels me in the pursuit of many interests, 
         hobbies, areas of study and artistic endeavors. I’m a fast 
-        learner, able to pick up new skills and juggle different 
-        projects and r</p>
+        learner, able to pick up new skills and adap quickly.</p>
         {children}
     </section>
   );
