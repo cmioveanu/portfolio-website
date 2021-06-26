6c299ddb2ccf7f1d2ead16d85d76af6d0fba3bfd
Cristian <cmioveanu@gmail.com>
2021-06-26 17:14:37 +0100

    add mobile menu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,49 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { header, mobileNav, desktopNav, openMenuButton, closeMenuButton } from './menu.module.css';
 
 
-const Menu = () => (
-  <header id="header">
-    <nav>
-      <ul>
-        <li><Link to="#header">Intro</Link></li>
+const Menu = () => {
+
+  const [showMobile, setShowMobile] = React.useState(false);
+
+  const hideMobile = () => setShowMobile(false);
+
+
+  return (
+    <header id={header}>
+
+      {!showMobile ? null : <nav id={mobileNav}>
+        <button id={closeMenuButton} onClick={hideMobile}>X</button>
+        <ul>
+          <li><Link to="#" onClick={hideMobile}>Intro</Link></li>
+          <li><Link to="#profile" onClick={hideMobile}>Profile</Link></li>
+          <li><Link to="#skills" onClick={hideMobile}>Skills</Link></li>
+          <li><Link to="#projects" onClick={hideMobile}>Projects</Link></li>
+          <li><Link to="#contact" onClick={hideMobile}>Contact</Link></li>
+        </ul></nav>}
+
+
+      <button id={openMenuButton} onClick={() => setShowMobile(true)}>
+        <svg viewBox="0 0 80 80" width="35" height="35">
+          <rect width="100" height="5"></rect>
+          <rect y="30" width="100" height="5"></rect>
+          <rect y="60" width="100" height="5"></rect>
+        </svg>
+      </button>
+
+
+      <nav id={desktopNav}><ul>
+        <li><Link to="#">Intro</Link></li>
         <li><Link to="#profile">Profile</Link></li>
         <li><Link to="#skills">Skills</Link></li>
         <li><Link to="#projects">Projects</Link></li>
         <li><Link to="#contact">Contact</Link></li>
-      </ul>
-    </nav>
-  </header >
-);
+      </ul></nav>
+
+    </header >
+  );
+};
 
 
 export default Menu;
